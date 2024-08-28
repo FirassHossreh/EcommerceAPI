8afaea061d759bc5b64a98ed5aad30e8a83d0acd
FirassHossreh <firass.hus@gmail.com>
2024-08-28 15:26:30 +0300

    mode ile ilgili duzeltme

--- middlewares/apiErrorMiddleware.js
+++ middlewares/apiErrorMiddleware.js
@@ -1,11 +1,24 @@
 const apiErrorMiddleware = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.statusText = err.statusText || "error";
+  if (process.env.ENVIRUMENT == "development") {
+    apiErrorDevelopment(err, res);
+  } else {
+    apiErrorProduction(err, res);
+  }
+};
+
+const apiErrorDevelopment = (err, res) => {
   res.status(err.statusCode).json({
     statusText: err.statusText,
     error: err,
     message: err.message,
   });
 };
-
+const apiErrorProduction = (err, res) => {
+  res.status(err.statusCode).json({
+    statusText: err.statusText,
+    message: err.message,
+  });
+};
 module.exports = apiErrorMiddleware;
